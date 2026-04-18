8e1785085711303c0509b7865efece3287cae5b1
coralcake <158809115+coralcake@users.noreply.github.com>
2026-04-18 15:09:56 +0200

    Update dark-light-mode.js

--- js/dark-light-mode.js
+++ js/dark-light-mode.js
@@ -3,7 +3,8 @@ window.onload = function() {
     const toggle1 = document.getElementById('modeToggle');
     const image1 = document.getElementById('modeImage');
     const image2 = document.getElementById('githubIcon');
-    const image3 = document.getElementById('xmarkIcon');
+    const image3 = document.getElementById('homeIcon');
+    const image4 = document.getElementById('xmarkIcon');
     
     // Sprawdzamy, czy stan trybu jest zapisany w localStorage
     const savedMode = localStorage.getItem('darkMode');
@@ -13,13 +14,15 @@ window.onload = function() {
         toggle1.checked = true;
         if (image1) image1.src = '/svg/mode-dark.svg';
         if (image2) image2.src = '/svg/GitHub_Invertocat_White.svg';
-        if (image3) image3.src = '/svg/xmark-night.svg';
+        if (image3) image3.src = '/svg/home-night.svg';
+        if (image4) image4.src = '/svg/xmark-night.svg';
         document.body.classList.add('dark-mode');
     } else {
         toggle1.checked = false;
         if (image1) image1.src = '/svg/mode-light.svg';
         if (image2) image2.src = '/svg/GitHub_Invertocat_Black.svg';
-        if (image3) image3.src = '/svg/xmark-day.svg';
+        if (image3) image3.src = '/svg/home-day.svg';
+        if (image4) image4.src = '/svg/xmark-day.svg';
         document.body.classList.remove('dark-mode');
     }
     
@@ -30,13 +33,15 @@ window.onload = function() {
             document.body.classList.add('dark-mode');
             if (image1) image1.src = '/svg/mode-dark.svg';
             if (image2) image2.src = '/svg/GitHub_Invertocat_White.svg';
-            if (image3) image3.src = '/svg/xmark-night.svg';
+            if (image3) image3.src = '/svg/home-night.svg';
+            if (image4) image4.src = '/svg/xmark-night.svg';
         } else {
             localStorage.setItem('darkMode', 'disabled');
             document.body.classList.remove('dark-mode');
             if (image1) image1.src = '/svg/mode-light.svg';
             if (image2) image2.src = '/svg/GitHub_Invertocat_Black.svg';
-            if (image3) image3.src = '/svg/xmark-day.svg';
+            if (image3) image3.src = '/svg/home-day.svg';
+            if (image4) image4.src = '/svg/xmark-day.svg';
         }
     });
 };
